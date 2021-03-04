abab67c74effe7aef97080e94d025f636aa7798e
vikramltts <78849038+vikramltts@users.noreply.github.com>
2021-03-04 22:14:34 +0530

    Create Hash.java

---
+++ Day5/Hash.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Hash {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		HashSet<String> hs = new HashSet<String>();
+		int n = Integer.parseInt(br.readLine());
+		for(int i=0;i<n;i++) {
+			hs.add(br.readLine());
+		}
+	System.out.println(hs.size());
+	}
+
+}
