f68698b3cec78c6c416361ccad3198b813f9700c
vikramltts <78849038+vikramltts@users.noreply.github.com>
2021-03-04 22:18:07 +0530

    Create Bowler.java

---
+++ day7/Bowler.java
@@ -0,0 +1,20 @@
+package Hash;
+
+public class Bowler {
+	private String name;
+
+	public Bowler(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
