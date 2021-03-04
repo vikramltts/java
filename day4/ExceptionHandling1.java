fa7d123207d2516b839bb1fadeed527281389916
vikramltts <78849038+vikramltts@users.noreply.github.com>
2021-03-04 22:24:43 +0530

    Create ExceptionHandling1.java

---
+++ day4/ExceptionHandling1.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class ExceptionHandling1 {
+
+	public static void main(String[] args) {
+		
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		// TODO Auto-generated method stub
+		try{
+			System.out.println("Enter the number of overs");
+			int n=Integer.parseInt(br.readLine());
+			System.out.println("Enter the number of runs for each over");
+			int arr[] = new int[n];
+			for(int i=0;i<arr.length;i++) {
+				arr[i]=Integer.parseInt(br.readLine());
+			}
+			System.out.println("Enter the over number");
+			int n1=Integer.parseInt(br.readLine());
+			System.out.println("Runs scored in this over" + arr[n1]);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+
+}
